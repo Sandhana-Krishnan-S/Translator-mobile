153f36cf65600fdf89c6d0df7ae8b7778809f695
Sandhana-Krishnan-S <sandhanakrishnansakthivel#gmail.com>
2024-07-02 19:58:42 +0530

    Loading screen while api call - Done

--- lib/View/home.dart
+++ lib/View/home.dart
@@ -29,6 +29,10 @@ class _HomePageState extends State<HomePage> {
   final _clientMessage = TextEditingController();
   final _serverMessage = TextEditingController();
 
+  //Loading
+
+  bool isLoading = false;
+
   @override
   void dispose() {
     _clientMessage.dispose();
@@ -76,197 +80,222 @@ class _HomePageState extends State<HomePage> {
       ),
       body: SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(right: 16.00 , left: 16.00 , top: 36.00 , bottom: 36.00),
-              decoration:  BoxDecoration(
-                color: Apptheme.secondaryColor,
-                borderRadius: BorderRadius.circular(12.00),
-              ),
-              child : Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Padding(padding: EdgeInsets.only(left: 16.00)),
-                  Container(
-                      alignment: Alignment.center,
-                      child: Row(
-                        children: [
-                          DropdownButtonHideUnderline(
-                            child: DropdownButton<String>(
-                              isDense: true,
-                              iconSize: 0.00,
-                              items: languageList.map((language, value) {
-                                return MapEntry(language , DropdownMenuItem<String>(
-                                  value: value,
-                                  child: Text(language),
-                                ));
-                              }).values.toList(),
-                              value: _language1,
-                              alignment: Alignment.center,
-                              onChanged: (String? newLanguage) {
-                                setState(() {
-                                  _language1 = newLanguage;
-                                  debugPrint(_language1);
-                                });
-                              },
-                              style: Styles.blackText(14),
-                              padding: EdgeInsets.zero,
-                            ),
-                          ),
-                        ],
-                      )
-                  ),
-                  Center(
-                    child: SvgPicture.asset(
-                      ImageUtils.transferArrow,
-                      height: 24,
-                    ),
-                  ),
-
-                  Container(
-                      alignment: Alignment.center,
-                      child: Row(
-                        children: [
-                          DropdownButtonHideUnderline(
-                            child: DropdownButton<String>(
-                                iconSize: 0.00,
-                                items: languageList.map((language, value) {
-                                  return MapEntry(language , DropdownMenuItem<String>(
-                                    value: value,
-                                    child: Text(language),
-                                  ));
-                                }).values.toList(),
-                                value: _language2,
-                                alignment: Alignment.center,
-                                onChanged: (String? newLanguage) {
-                                  setState(() {
-                                    _language2 = newLanguage;
-                                  });
-                                },
-                                style: Styles.blackText(14)
-                            ),
-                          ),
-                        ],
-                      )
-                  ),
-                  const Padding(padding: EdgeInsets.only(right: 16.00)),
-                ],
-              ) ,
-            ),
-            Column(
+        child: Stack(
+          children:[
+            Positioned(
+              // width: MediaQuery.of(context).size.width,
+              // height: MediaQuery.of(context).size.height,
+              child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
-                  margin: const EdgeInsets.only(right: 16.0, left: 16.0, top: 16.0, bottom: 16.0),
-                  padding: const EdgeInsets.all(16),
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  height: MediaQuery.of(context).size.width * 0.6,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Apptheme.lightGrey),
-                    borderRadius: const BorderRadius.all(Radius.circular(12.0)),
+                  margin: const EdgeInsets.only(right: 16.00 , left: 16.00 , top: 36.00 , bottom: 36.00),
+                  decoration:  BoxDecoration(
+                    color: Apptheme.secondaryColor,
+                    borderRadius: BorderRadius.circular(12.00),
                   ),
-                  child: Stack(
+                  child : Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      TextField(
-                        controller: _clientMessage,
-                        keyboardType: TextInputType.multiline,
-                        minLines: 5,
-                        maxLines: null,
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                        ),
-                      ),
-                      Positioned(
-                        right: 0,
-                        bottom: 0,
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            final translatedMessage = await TranslateApiHandler.translateMessage(
-                              _clientMessage.text,
-                              _language1.toString(),
-                              _language2.toString(),
-                            );
-                            setState(() {
-                              _serverMessage.text = translatedMessage;
-                            });
-                          },
-                          style: Styles.primaryButton(),
-                          child: Text(
-                            Constants.translator,
-                            textAlign: TextAlign.center,
-                            style: Styles.blackText(12),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(right: 16.0, left: 16.0, top: 16.0, bottom: 16.0),
-                  padding: const EdgeInsets.all(16),
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  height: MediaQuery.of(context).size.width * 0.6,
-                  decoration: Styles.textBox(),
-                  child: Stack(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 24.0),
-                        child: TextField(
-                          controller: _serverMessage,
-                          keyboardType: TextInputType.multiline,
-                          minLines: 5,
-                          maxLines: null,
-                          decoration: const InputDecoration(
-                            border: InputBorder.none,
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        top: 0,
-                        left: 0,
-                        child: Row(
-                          children: [
-                            SvgPicture.asset(ImageUtils.universalLanguage),
-                            const Padding(padding: EdgeInsets.only(left: 16)),
-                            Text(
-                              DropdownHelper.findKeyByValue(_language2.toString(), languageList),
-                              style: Styles.primaryText(),
-                            ),
-                            const Padding(padding: EdgeInsets.only(bottom: 24))
-                          ],
-                        ),
-                      ),
-                      Positioned(
-                        right: 0,
-                        bottom: 0,
-                        child: ElevatedButton(
-                          onPressed: () => {
-                            FlutterClipboard.copy(_serverMessage.text).then((value) => {
-                              //todo : add a way to show the copy worked by changing the copy button
-                            })
-                          },
-                          style: Styles.customTextButton(),
+                      const Padding(padding: EdgeInsets.only(left: 16.00)),
+                      Container(
+                          alignment: Alignment.center,
                           child: Row(
                             children: [
-                              SvgPicture.asset(ImageUtils.copyLogo) ,
-                              const Padding(padding: EdgeInsets.only(left: 12.00)),
-                              Text(
-                                Constants.copy,
+                              DropdownButtonHideUnderline(
+                                child: DropdownButton<String>(
+                                  isDense: true,
+                                  iconSize: 0.00,
+                                  items: languageList.map((language, value) {
+                                    return MapEntry(language , DropdownMenuItem<String>(
+                                      value: value,
+                                      child: Text(language),
+                                    ));
+                                  }).values.toList(),
+                                  value: _language1,
+                                  alignment: Alignment.center,
+                                  onChanged: (String? newLanguage) {
+                                    setState(() {
+                                      _language1 = newLanguage;
+                                      debugPrint(_language1);
+                                    });
+                                  },
+                                  style: Styles.blackText(14),
+                                  padding: EdgeInsets.zero,
+                                ),
+                              ),
+                            ],
+                          )
+                      ),
+                      Center(
+                        child: SvgPicture.asset(
+                          ImageUtils.transferArrow,
+                          height: 24,
+                        ),
+                      ),
+
+                      Container(
+                          alignment: Alignment.center,
+                          child: Row(
+                            children: [
+                              DropdownButtonHideUnderline(
+                                child: DropdownButton<String>(
+                                    iconSize: 0.00,
+                                    items: languageList.map((language, value) {
+                                      return MapEntry(language , DropdownMenuItem<String>(
+                                        value: value,
+                                        child: Text(language),
+                                      ));
+                                    }).values.toList(),
+                                    value: _language2,
+                                    alignment: Alignment.center,
+                                    onChanged: (String? newLanguage) {
+                                      setState(() {
+                                        _language2 = newLanguage;
+                                      });
+                                    },
+                                    style: Styles.blackText(14)
+                                ),
+                              ),
+                            ],
+                          )
+                      ),
+                      const Padding(padding: EdgeInsets.only(right: 16.00)),
+                    ],
+                  ) ,
+                ),
+                Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(right: 16.0, left: 16.0, top: 16.0, bottom: 16.0),
+                      padding: const EdgeInsets.all(16),
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      height: MediaQuery.of(context).size.width * 0.6,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Apptheme.lightGrey),
+                        borderRadius: const BorderRadius.all(Radius.circular(12.0)),
+                      ),
+                      child: Stack(
+                        children: [
+                          TextField(
+                            controller: _clientMessage,
+                            keyboardType: TextInputType.multiline,
+                            minLines: 5,
+                            maxLines: null,
+                            decoration: const InputDecoration(
+                              border: InputBorder.none,
+                            ),
+                          ),
+                          Positioned(
+                            right: 0,
+                            bottom: 0,
+                            child: ElevatedButton(
+                              onPressed: () async {
+                                setState(() {
+                                  isLoading = true;
+                                });
+                                final translatedMessage = await TranslateApiHandler.translateMessage(
+                                  _clientMessage.text,
+                                  _language1.toString(),
+                                  _language2.toString(),
+                                );
+                                setState(() {
+                                  _serverMessage.text = translatedMessage;
+                                });
+                                setState(() {
+                                  isLoading = false;
+                                });
+                              },
+                              style: Styles.primaryButton(),
+                              child: Text(
+                                Constants.translator,
                                 textAlign: TextAlign.center,
                                 style: Styles.blackText(12),
                               ),
-                            ],
+                            ),
                           ),
-                        ),
+                        ],
                       ),
-                    ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(right: 16.0, left: 16.0, top: 16.0, bottom: 16.0),
+                      padding: const EdgeInsets.all(16),
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      height: MediaQuery.of(context).size.width * 0.6,
+                      decoration: Styles.textBox(),
+                      child: Stack(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 24.0),
+                            child: TextField(
+                              controller: _serverMessage,
+                              keyboardType: TextInputType.multiline,
+                              minLines: 5,
+                              maxLines: null,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            top: 0,
+                            left: 0,
+                            child: Row(
+                              children: [
+                                SvgPicture.asset(ImageUtils.universalLanguage),
+                                const Padding(padding: EdgeInsets.only(left: 16)),
+                                Text(
+                                  DropdownHelper.findKeyByValue(_language2.toString(), languageList),
+                                  style: Styles.primaryText(),
+                                ),
+                                const Padding(padding: EdgeInsets.only(bottom: 24))
+                              ],
+                            ),
+                          ),
+                          Positioned(
+                            right: 0,
+                            bottom: 0,
+                            child: ElevatedButton(
+                              onPressed: () => {
+                                FlutterClipboard.copy(_serverMessage.text).then((value) => {
+                                  //todo : add a way to show the copy worked by changing the copy button
+                                })
+                              },
+                              style: Styles.customTextButton(),
+                              child: Row(
+                                children: [
+                                  SvgPicture.asset(ImageUtils.copyLogo) ,
+                                  const Padding(padding: EdgeInsets.only(left: 12.00)),
+                                  Text(
+                                    Constants.copy,
+                                    textAlign: TextAlign.center,
+                                    style: Styles.blackText(12),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const Padding(padding: EdgeInsets.only(bottom: 24.00, right: 16.00)),
+              ],
+              ),
+            ),
+            if (isLoading)
+              Positioned.fill(
+                child: Container(
+                  color: Colors.black.withOpacity(0.2),
+                  child: const Center(
+                    child: CircularProgressIndicator(
+                      color: Apptheme.primaryColor,
+                    ),
                   ),
                 ),
-              ],
-            ),
-            const Padding(padding: EdgeInsets.only(right: 16.00)),
-          ],
+              ),
+        ],
         ),
       ),
     );
